ebe46892c7d83d6e5361e37c244cbd2c449d8358
A-d-i-t-y-a-2-5 <adityapratapsaha@gmail.com>
2025-09-27 06:20:49 +0000

    Refactor data processing: replace CSV with Pickle for data loading, add argument parsing for sample size

--- expand_data.py
+++ expand_data.py
@@ -1,7 +1,49 @@
+import argparse
+import os
 import pandas as pd
 
-df = pd.read_csv("data/processed/500/web_traffic_sample.csv")
 
-df["series_value"] = df["series_value"].str.replace("<NumpyExtensionArray>\n", "")
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n-samples",
+        type=int,
+        default=500,
+        help="Number of samples to extract from the dataset",
+    )
+    return parser.parse_args()
 
-print(df["series_value"].iloc[0])
+
+def main():
+    args = parse_args()
+
+    df = pd.read_pickle(
+        os.path.join("data", "processed", str(args.n_samples), "web_traffic_sample.pkl")
+    )
+    df["timestamps"] = df.apply(
+        lambda row: pd.date_range(
+            start=row["start_timestamp"], periods=len(row["series_value"]), freq="D"
+        ),
+        axis=1,
+    )
+    
+    df = df[["series_name", "timestamps", "series_value"]]
+
+    long_df = df.explode(["timestamps", "series_value"]).reset_index(drop=True)
+    long_df["series_value"] = long_df["series_value"].astype(float)
+
+    pivot_df = long_df.pivot(
+        index="timestamps", columns="series_name", values="series_value"
+    )
+
+    pivot_df.to_csv(
+        os.path.join(
+            "data", "processed", str(args.n_samples), "web_traffic_expanded.csv"
+        )
+    )
+
+    pivot_df.info()
+
+
+if __name__ == "__main__":
+    main()
